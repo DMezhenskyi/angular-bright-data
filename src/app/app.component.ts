21d18903a207deb1cc698ceffb3062416b1aaf51
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2023-10-16 13:05:39 +0200

    initial state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { of } from 'rxjs';
+
+interface AmazonDeal {
+  title: string;
+  link: string;
+  img: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-bright-data';
+  items$ = of<AmazonDeal[]>([]);
 }
